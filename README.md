7039a48c8f990da634e1a32da41c5f48fe5bd580
Vadym Korshunov <vaden4d@gmail.com>
2019-03-26 00:17:05 +0200

    fixed service

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cloudinfra
 Cloud infrastructure tasks at UCU (2019)
+
+## 1. Dockerfile + service
+Run application locally:
+
+- clone repository: `git clone https://github.com/Vaden4d/cloudinfra.git`
+- run `docker build -t hm-korshunov .`
+- run `docker run -d -p 5000:5000 --rm --name cloud-infra hm-korshunov`
+
+Then go to `localhost:5000` and read the internal content. 
+To stop and remove container run:
+
+`docker container stop $(docker container ls -f "name=cloud-infra" -q)` .
--- app/load_service.py
+++ app/load_service.py
@@ -16,16 +16,27 @@ def factorization(n):
 
 @app.route('/')
 def highload_probe():
-    diff = int(request.args.get('time', ''))
     x = 191666634999089
+    diff = int(request.args.get('time', '1'))
+    number = int(request.args.get('number', str(x)))
     start = time.time()
     b = True
     while b:
-        factorization(x)
+        l = factorization(number)
         end = time.time()
         if (end - start > diff):
             b = False
-    return 'High load probe done'
+
+    strng = 'Hi! This is service for factorization of the number 191666634999089'
+    strng += ' by default. <br> I like minimalism without huge GUI, so you can go by the link '
+    strng += 'localhost:5000/?time=30&number=191666634999089 and rerun the '
+    strng += 'computations as long as you want! <br>'
+    strng += 'You can change the argument near time (seconds) and desired number (number)'
+    strng += ' and run your own computations! <br>'
+    strng += 'If you see that hence high load probe done! <br>'
+    strng += '<br>'
+    strng += 'Factorized list for ' + str(number) + ' is ' + str(l)
+    return strng
 
 @app.route("/livetest")
 def livetest():
